2d47857400a33c87a31154c7aeb29cae442af989
Ibraam Magdy <112469519+ibraam123@users.noreply.github.com>
2026-04-25 23:50:32 +0300

    Add files via upload

---
+++ 3- Components - Navbar  (1).js
@@ -0,0 +1,13 @@
+JavaScript
+document.write(`
+    <div class="navbar">
+        <h2>Task Manager</h2>
+        <div class="nav-links">
+            <a href="dashboard.html">Dashboard</a>
+            <a href="taskboard.html">Task Board</a>
+            <a href="chat.html">Chat</a>
+            <a href="files.html">Files</a>
+            <a href="login.html">Logout</a>
+        </div>
+    </div>
+`);
